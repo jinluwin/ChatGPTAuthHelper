b3a049fa177c4532c2f8973cd1708acf0eb338d7
pengzhile <pengzhile@gmail.com>
2023-05-19 21:59:45 +0800

    support PKCE
    Signed-off-by: pengzhile <pengzhile@gmail.com>

--- src/background.js
+++ src/background.js
@@ -1,20 +1,12 @@
-function blockRequest(details) {
-    console.log("Blocked: ", details.url);
-    return {
-        cancel: true
-    };
-}
-
-if (chrome.webRequest.onBeforeRequest.hasListener(blockRequest)) {
-    chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
-}
-
-try {
-    chrome.webRequest.onBeforeRequest.addListener(
-        blockRequest,
-        {urls: ['https://chat.openai.com/api/auth/callback/auth0?*']},
-        ['blocking']
-    );
-} catch (e) {
-    console.error(e);
-}
+chrome.webRequest.onBeforeRedirect.addListener(
+    function (details) {
+        if (details.redirectUrl.startsWith('com.openai.chat://')) {
+            chrome.storage.local.set({url: details.redirectUrl}, function () {
+                chrome.tabs.update(details.tabId, {url: 'auth.html'});
+            });
+            return {cancel: true};
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["responseHeaders"]
+);
